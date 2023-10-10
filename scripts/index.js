b4aba7c0fe63210f36157ef087be977ea76e76d0
Anna Haratova <black-isida@yandex.ru>
2023-10-10 13:30:58 +0300

    deploy to GH-pages

---
+++ scripts/index.js
@@ -0,0 +1,8 @@
+function openPopup() {
+    document.getElementById("popup-menu").style.visibility = 'visible';
+  }
+  
+  function closePopup() {
+    document.getElementById("popup-menu").style.visibility = 'hidden';
+  }
+  
